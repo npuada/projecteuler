a451f5a318779a1e15ebf28d1440f5e6971fde49
Nix <nixpuada@gmail.com>
2013-04-04 21:51:43 +0800

    Added solution to Problem 41 (Pandigital prime)

---
+++ Problem41.py
@@ -0,0 +1,16 @@
+import itertools
+import tools
+
+primes = tools.getPrimes(2766)
+primes.reverse()
+
+if __name__ == '__main__':
+	for p in itertools.permutations(reversed(range(1,8))):
+		if p[6] %2 == 1:
+			num = p[0]*1000000 + p[1]*100000 + p[2]*10000 + p[3]*1000 + p[4]*100 + p[5]*10 + p[6]
+			for p in primes:
+				if num%p == 0:
+					break
+			else:
+				print(num)
+				break
