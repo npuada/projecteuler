0882c944bbee847e0e6e0dfa7073d2aaadd7ea7f
Nix <nixpuada@gmail.com>
2013-04-04 21:53:15 +0800

    Added solution to Problem 38 (Pandigital multiples)

---
+++ Problem38.py
@@ -0,0 +1,14 @@
+import itertools
+import tools
+
+if __name__ == '__main__':
+	lst = list(range(9,0,-1))
+	perms = itertools.permutations(lst)
+	
+	for p in perms:
+		x = p[0]*1000 + p[1]*100 + p[2]*10 + p[3]
+		y = p[4] *10000 + p[5]*1000 + p[6]*100 + p[7]*10 + p[8]
+
+		if tools.gcd(x,y) == x and y/2 == x:
+			print(x*100000+y)
+			break
