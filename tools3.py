d8c3ba3523c718bc9fba6cfaddc3f8a82699c76f
Nix <nixpuada@gmail.com>
2013-04-13 22:10:52 +0800

    Added tools3 (for Python 3.x solutions)

---
+++ tools3.py
@@ -0,0 +1,38 @@
+import math
+
+def getPrimes(limit):
+	if limit < 2:
+		return []
+
+	#odd = [i for i in xrange(3, limit, 2)]
+	primeMap = {i: True for i in range(3, limit, 2)}
+	
+	primeList = [2]
+	stop = math.floor(math.sqrt(limit))
+	for i in range(3, limit, 2):
+		key = i**2
+		while key < limit:
+			primeMap[key] = False
+			key += (i*2)
+		if i > stop:
+			break
+
+	for key in primeMap:
+		if primeMap[key]:
+			primeList.append(key)
+	return primeList
+
+def gcd(a,b):
+	if a == 0 or b == 0:
+		return abs(a-b)
+
+	while a != 0 and b != 0:
+		if a > b:
+			a %= b
+		elif b >= a:
+			b %= a
+
+	if a > b:
+		return a
+	else:
+		return b
